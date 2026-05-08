b6a0dfa42b19d743846c2b724da564914282bef5
Kasiaiadam <kadasie@kasiaiadams-mbp.lan>
2026-05-08 21:20:25 +0200

    rename structure

--- build_complete.py
+++ build_complete.py
@@ -10,13 +10,13 @@ def run(cmd):
 
 def main():
     # 1st pass (generate .dat with page numbers)
-    run("tectonic masterFiles/main.tex")
+    run("tectonic masterDocument/main.tex")
 
     # generate tree from mytoc.dat
-    run("python masterFiles/generate_tree.py")
+    run("python masterDocument/generate_tree.py")
 
     # 2nd pass (include generated tree)
-    run("tectonic masterFiles/main.tex")
+    run("tectonic masterDocument/main.tex")
 
 if __name__ == "__main__":
     main()
