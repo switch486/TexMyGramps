1d1b0298f9b69cb0c14a5b370ccc958a47bbe3e1
Kasiaiadam <kadasie@kasiaiadams-mbp.lan>
2026-05-08 15:56:05 +0200

    cleanup

--- prepare_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-# Prepare the development environment for GrampsBook.
-# Source the local Python virtual environment and extend PATH for Tectonic tooling.
-
-source /Users/kadasie/PRIV_PRIV/GrampsBook/.venv/bin/activate
-export PATH="$PATH:/Users/kadasie/tectonicStuff"
-
-echo "Development environment prepared."
